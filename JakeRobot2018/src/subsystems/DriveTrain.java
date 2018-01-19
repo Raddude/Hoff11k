b675de87391322966cc8bccf0943a8ccedf529f5
1787 Orange Robotics <1787 Orange Robotics@DESKTOP-KEBDUTV.orange.local>
2018-01-19 10:15:38 -0500

    lol who even reads these

---
+++ JakeRobot2018/src/subsystems/DriveTrain.java
@@ -0,0 +1,74 @@
+package subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.hal.HAL;
+import edu.wpi.first.wpilibj.hal.FRCNetComm.tInstances;
+import edu.wpi.first.wpilibj.hal.FRCNetComm.tResourceType;
+
+public class DriveTrain {
+	
+	private final int FRONT_RIGHT_TALON_ID = 8;
+	private final int REAR_RIGHT_TALON_ID = 9;
+	private final int FRONT_LEFT_TALON_ID = 6;
+	private final int REAR_LEFT_TALON_ID = 7;
+	private WPI_TalonSRX frontLeftMotor = new WPI_TalonSRX(FRONT_LEFT_TALON_ID);
+	private WPI_TalonSRX backLeftMotor = new WPI_TalonSRX(REAR_LEFT_TALON_ID);	
+	private WPI_TalonSRX frontRightMotor = new WPI_TalonSRX(FRONT_RIGHT_TALON_ID);
+	private WPI_TalonSRX backRightMotor = new WPI_TalonSRX(REAR_RIGHT_TALON_ID);
+	
+	private SpeedControllerGroup leftMotors;
+	private SpeedControllerGroup rightMotors;
+	
+	private DifferentialDrive myDrive;
+
+	public DriveTrain() {
+		frontLeftMotor.setInverted(true);
+		backLeftMotor.setInverted(true);
+		frontRightMotor.setInverted(false);
+		backRightMotor.setInverted(false);
+		//leftMotors = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
+		//rightMotors = new SpeedControllerGroup(frontRightMotor, backRightMotor);
+		//myDrive = new DifferentialDrive(leftMotors, rightMotors);
+	}
+	
+	  @SuppressWarnings("ParameterName")
+	  public void arcadeDrive(double xSpeed, double zRotation) {
+
+	    double leftMotorOutput;
+	    double rightMotorOutput;
+
+	    double maxInput = Math.copySign(Math.max(Math.abs(xSpeed), Math.abs(zRotation)), xSpeed);
+
+	    if (xSpeed >= 0.0) {
+	      // First quadrant, else second quadrant
+	      if (zRotation >= 0.0) {
+	        leftMotorOutput = maxInput;
+	        rightMotorOutput = xSpeed - zRotation;
+	      } else {
+	        leftMotorOutput = xSpeed + zRotation;
+	        rightMotorOutput = maxInput;
+	      }
+	    } else {
+	      // Third quadrant, else fourth quadrant
+	      if (zRotation >= 0.0) {
+	        leftMotorOutput = xSpeed + zRotation;
+	        rightMotorOutput = maxInput;
+	      } else {
+	        leftMotorOutput = maxInput;
+	        rightMotorOutput = xSpeed - zRotation;
+	      }
+	    }
+	    
+	    frontLeftMotor.set(leftMotorOutput);
+	    backLeftMotor.set(leftMotorOutput);
+	    System.out.println("Left: " + leftMotorOutput);
+	    frontRightMotor.set(rightMotorOutput);
+	    backRightMotor.set(rightMotorOutput);
+	    System.out.println("Right: " + rightMotorOutput);
+	  }
+
+}
---
+++ JakeRobot2018/src/subsystems/Flywheel.java
@@ -0,0 +1,7 @@
+package subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Flywheel {
+	
+}
